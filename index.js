2f1d0cb9c3e14c7ecad6449b73fbcbf0d3fb54a9
davidrodjen <davidrodriguezjenkins@gmail.com>
2020-03-16 13:10:36 -0700

    fixed foreach

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ window.addEventListener("load", () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
 
-
+    console.log(sounds);
 
     //ForEach utilizies all the sounds in the collection created above
-    pads.ForEach((pad, index) => {
+    pads.forEach((pad, index) => {
         pad.addEventListener("click", function(){
             sounds[index].play();
         });
